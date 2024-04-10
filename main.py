124c35cacaee8f0b7834bae60697dc61b716eb0c
Topher7lol <157950427+Topher7lol@users.noreply.github.com>
2024-04-10 12:35:41 +1200

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ value = 1
 while True:
   print(f"{value}.")
   value += 1
+
+  if value == 1000000:
+    print("Your food is done cooking!")
+    option = input("Continue Counting/Cooking y/n: ")
+
+    if option == 'yes' or 'Yes' or 'YES' or 'Y' or 'y':
+      while True:
+        print(f"{value}.")
+        value += 1
+    elif option == 'no' or 'No' or 'NO' or 'N' or 'n'
+        print("Done!")
