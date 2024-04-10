864ff71e579a4275322fc4c206c6267911897b12
Topher7lol <157950427+Topher7lol@users.noreply.github.com>
2024-04-11 10:09:33 +1200

    Update main.py
    fixed bug

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import random
 value = 1
 
 hi = True
+printing = True
 
-while hi == True:
+while hi == True and printing == True:
   print(f"{value}.")
   value += 1
   while_cooking = True
   if value == 1000000:
     hi = False
+    while_cooking = False
+    printing = False
   
-if value == 1000000 and hi == False:
+if value == 1000000 and hi == False and printing == False:
   print(f"Your food is done cooking! ({value})")
   while_cooking = False
   option = input("Continue Counting/Cooking y/n: ")
